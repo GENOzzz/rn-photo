0d508d79422f88b62e2599a600fc45114e0fe21a
GENO_P <92358313+GENOzzz@users.noreply.github.com>
2023-02-15 00:29:46 +0900

    input Components 생성
    1.컴포넌트 생성
    2.간소화(EMAIL, PASSWORD만 받음)

---
+++ src/components/Input.js
@@ -0,0 +1,105 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { forwardRef, useState } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { BLACK } from '../colors';
+
+export const ReturnKeyTypes = {
+  DONE: 'done',
+  NEXT: 'next',
+};
+
+export const InputTypes = {
+  EMAIL: 'EMAIL',
+  PASSWRD: 'PASSWORD',
+};
+
+const InputTypeProps = {
+  EMAIL: {
+    title: 'EMAIL',
+    placeholder: 'your@email.com',
+    keyboardType: 'email-address',
+    secureTextEntry: false,
+    iconName: { active: 'email', inactive: 'email-outline' },
+  },
+  PASSWORD: {
+    title: 'PASSWORD',
+    placeholder: 'PASSWORD',
+    keyboardType: 'default',
+    secureTextEntry: true,
+    iconName: { active: 'lock', inactive: 'lock-outline' },
+  },
+};
+
+const Input = forwardRef(({ inputType, ...props }, ref) => {
+  const {
+    title,
+    placeholder,
+    keyboardType,
+    secureTextEntry,
+    iconName: { active, inactive },
+  } = InputTypeProps[inputType];
+
+  const [isFocused, setIsFocused] = useState(false);
+  return (
+    <View style={defaultStyles.container}>
+      <Text style={defaultStyles.title}>{title}</Text>
+      <View>
+        <TextInput
+          ref={ref}
+          {...props}
+          placeholder={placeholder}
+          keyboardType={keyboardType}
+          secureTextEntry={secureTextEntry}
+          onFocus={() => {
+            setIsFocused(true);
+          }}
+          onBlur={() => {
+            setIsFocused(false);
+          }}
+          style={[defaultStyles.input]}
+          textContentType="none"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <View style={defaultStyles.icon}>
+          <MaterialCommunityIcons
+            name={isFocused ? active : inactive}
+            size={24}
+            color={BLACK}
+          />
+        </View>
+      </View>
+    </View>
+  );
+});
+Input.displayName = 'Input';
+
+Input.propTypes = {
+  inputType: PropTypes.oneOf(Object.values(InputTypes)).isRequired,
+};
+
+const defaultStyles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  title: {
+    marginBottom: 4,
+    fontWeight: '700',
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderRadius: 8,
+    height: 42,
+    paddingHorizontal: 10,
+    paddingLeft: 40,
+  },
+  icon: {
+    position: 'absolute',
+    left: 8,
+    height: '100%',
+    justifyContent: 'center',
+  },
+});
+
+export default Input;
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,13 +1,23 @@
 import { useNavigation } from '@react-navigation/native';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import Input, {
+  InputTypes,
+  keyboardTypes,
+  ReturnKeyTypes,
+} from '../components/Input';
 import { AuthRoutes } from '../navigations/routes';
 
 const SignInScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={StyleSheet.container}>
+    <View style={styles.container}>
       <Text>Sign In</Text>
+      <Input inputType={InputTypes.EMAIL} returnKeyType={ReturnKeyTypes.NEXT} />
+      <Input
+        inputType={InputTypes.PASSWRD}
+        returnKeyType={ReturnKeyTypes.DONE}
+      />
       <Button
         title="Sign Up"
         onPress={() => navigation.navigate(AuthRoutes.SIGN_UP)}
