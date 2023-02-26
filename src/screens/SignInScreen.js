a1abaeb9cb532be0b349066a5baf828ecff1d4ce
GENO_P <92358313+GENOzzz@users.noreply.github.com>
2023-02-26 15:15:34 +0900

    로그인 화면구성
    1.텍스트 버튼 컴포넌트 구현(보통 페이지 이동)
    2.HR컴포넌트 구현

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,54 +1,104 @@
 import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Image, Keyboard, StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+
 import Button from '../components/Button';
+import HR from '../components/HR';
 import Input, {
   InputTypes,
   keyboardTypes,
   ReturnKeyTypes,
 } from '../components/Input';
 import SafeInputView from '../components/SafeInputView';
+import TextButton from '../components/TextButton';
 import { AuthRoutes } from '../navigations/routes';
+import { WHITE } from '../colors';
 
 const SignInScreen = () => {
   const navigation = useNavigation();
-  const { top } = useSafeAreaInsets();
+  const { top, bottom } = useSafeAreaInsets();
+
+  const passwordRef = useRef();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    setDisabled(!email || !password);
+  }, [email, password]);
+
+  const onSubmit = () => {
+    Keyboard.dismiss();
+    if (!disabled && !isLoading) {
+      setIsLoading(true);
+      console.log(email, password);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <SafeInputView>
+      <StatusBar style="light" />
       <View style={[styles.container, { padding: top }]}>
-        <Text>Sign In</Text>
-        <Input
-          styles={InputStyles}
-          value={email}
-          onChangeText={(text) => {
-            setEmail(text.trim());
-          }}
-          inputType={InputTypes.EMAIL}
-          returnKeyType={ReturnKeyTypes.NEXT}
-        />
-        <Input
-          styles={InputStyles}
-          value={password}
-          onChangeText={(text) => {
-            setPassword(text.trim());
-          }}
-          inputType={InputTypes.PASSWRD}
-          returnKeyType={ReturnKeyTypes.DONE}
-        />
-        <Button
-          title="Sign Up"
-          onPress={() => navigation.navigate(AuthRoutes.SIGN_UP)}
-          styles={{
-            container: {
-              paddingHorizontal: 20,
-              marginTop: 20,
-            },
-          }}
-        />
+        <View style={StyleSheet.absoluteFill}>
+          <Image
+            source={require('../../assets/cover.png')}
+            style={{ width: '100%' }}
+            resizeMode="cover"
+          />
+        </View>
+        <View
+          style={[styles.form, { paddingBottom: bottom ? bottom + 10 : 40 }]}
+        >
+          <Text>Sign In</Text>
+          <Input
+            value={email}
+            onChangeText={(text) => {
+              setEmail(text.trim());
+            }}
+            inputType={InputTypes.EMAIL}
+            returnKeyType={ReturnKeyTypes.NEXT}
+            onSubmitEditing={() => {
+              passwordRef.current.focus();
+            }}
+            styles={{ container: { marginBottom: 20 } }}
+          />
+          <Input
+            ref={passwordRef}
+            value={password}
+            onChangeText={(text) => {
+              setPassword(text.trim());
+            }}
+            inputType={InputTypes.PASSWRD}
+            returnKeyType={ReturnKeyTypes.DONE}
+            onSubmitEditing={onSubmit}
+            styles={{ container: { marginBottom: 20 } }}
+          />
+          <Button
+            title="로그인"
+            onPress={onSubmit}
+            disabled={disabled}
+            isLoading={isLoading}
+            styles={{
+              container: {
+                marginBottom: 20,
+              },
+            }}
+          />
+
+          <HR text={'OR'} styles={{ container: { marginVertical: 30 } }}></HR>
+
+          <TextButton
+            title={'회원가입'}
+            onPress={() => {
+              navigation.navigate(AuthRoutes.SIGN_UP);
+            }}
+          />
+        </View>
       </View>
     </SafeInputView>
   );
@@ -57,8 +107,15 @@ const SignInScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  form: {
     alignItems: 'center',
+    backgroundColor: WHITE,
+    paddingHorizontal: 20,
+    paddingTop: 40,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
 });
 
