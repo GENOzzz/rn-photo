c74401a706d596b483087de2ba29e6d7226242a5
GENO_P <92358313+GENOzzz@users.noreply.github.com>
2023-03-06 23:30:23 +0900

    이미지 캐싱
    1.expo-splash-screen 추가
    2.Navigation.js 첫 화면 async await 추가

--- src/navigations/Navigation.js
+++ src/navigations/Navigation.js
@@ -1,9 +1,35 @@
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './AuthStack';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
 
 const Navigation = () => {
+  const [isReady, setIsready] = useState(false);
+
+  const onReady = async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsready(true);
+      }
+    })();
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={onReady}>
       <AuthStack />
     </NavigationContainer>
   );
