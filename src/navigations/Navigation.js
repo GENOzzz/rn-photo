88b6aada1df7b0c0660d1f7e911aef966cd628bc
GENO_P <92358313+GENOzzz@users.noreply.github.com>
2023-03-07 22:23:53 +0900

    로그인 시 이미지 asset 추가

--- src/navigations/Navigation.js
+++ src/navigations/Navigation.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './AuthStack';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
+import { Asset } from 'expo-asset';
 
 const Navigation = () => {
   const [isReady, setIsready] = useState(false);
@@ -16,7 +17,11 @@ const Navigation = () => {
     (async () => {
       try {
         await SplashScreen.preventAutoHideAsync();
+        await Asset.fromModule(
+          require('../../assets/cover.png')
+        ).downloadAsync();
       } catch (e) {
+        // eslint-disable-next-line no-console
         console.log(e);
       } finally {
         setIsready(true);
